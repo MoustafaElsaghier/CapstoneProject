60caa18b40d649c108306642f583f41df90d03a0
M_ELSaghier <thedreamereng@gmail.com>
2018-01-30 16:14:01 +0200

    Implement Restaurants Details Activity Code

--- app/src/main/java/elsaghier/developer/com/capstoneproject/RestaurantsDetailsActivityFragment.java
+++ app/src/main/java/elsaghier/developer/com/capstoneproject/RestaurantsDetailsActivityFragment.java
@@ -1,15 +1,37 @@
 package elsaghier.developer.com.capstoneproject;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+import butterknife.BindView;
+import elsaghier.developer.com.capstoneproject.Models.RestaurantModel;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class RestaurantsDetailsActivityFragment extends Fragment {
+
+    @BindView(R.id.restaurant_detail_name)
+    TextView mName;
+    @BindView(R.id.rest_detail_location)
+    TextView mLocation;
+
+    @BindView(R.id.item_price)
+    TextView mItemPrice;
+    GoogleMap map;
+
+    @BindView(R.id.rest_detail_img)
+    ImageView imgImageView;
 
     public RestaurantsDetailsActivityFragment() {
     }
@@ -18,5 +40,30 @@ public class RestaurantsDetailsActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_restaurants_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RestaurantModel restaurantModel = (RestaurantModel) getActivity().getIntent().getSerializableExtra("rest_item");
+
+        double lat = Double.parseDouble(restaurantModel.getLocation().getLatitude());
+        double lng = Double.parseDouble(restaurantModel.getLocation().getLongitude());
+
+        LatLng restLocation = new LatLng(lat, lng);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(restLocation, 13));
+//
+//        map.addMarker(new MarkerOptions()
+//                .title(restaurantModel.getName())
+//                .snippet("The most populous Restaurant in city.")
+//                .position(restLocation));
+
+        mName.setText(restaurantModel.getName());
+
+        String itemPrice = String.format("%s %s", restaurantModel.getPriceRange(), restaurantModel.getCurrency());
+        mItemPrice.setText(itemPrice);
+
+        mLocation.setText(restaurantModel.getLocation().getAddress());
+        Glide.with(this).load(restaurantModel.getFeaturedImage()).into(imgImageView);
     }
 }
