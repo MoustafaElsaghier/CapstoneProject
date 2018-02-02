5096a2042eb27e397231b3089967daf6fbb5812c
M_ELSaghier <thedreamereng@gmail.com>
2018-02-02 23:11:51 +0200

    generate keystore

--- app/src/main/java/elsaghier/developer/com/capstoneproject/Activities/ToDoActivity.java
+++ app/src/main/java/elsaghier/developer/com/capstoneproject/Activities/ToDoActivity.java
@@ -13,6 +13,7 @@ public class ToDoActivity extends AppCompatActivity {
 
     ToDoDialog doDialog;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
