594d62ef26ea7034d0db155bdbeb9ad4ed8b6874
M_ELSaghier <thedreamereng@gmail.com>
2018-02-03 17:26:26 +0200

    create row item for to do list and its model

---
+++ app/src/main/java/elsaghier/developer/com/capstoneproject/Models/ToDoModel.java
@@ -0,0 +1,21 @@
+package elsaghier.developer.com.capstoneproject.Models;
+
+/**
+ * Created by ELSaghier on 2/3/2018.
+ */
+
+public class ToDoModel {
+    private String item;
+
+    public ToDoModel(String item) {
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+}
