d285c35f87f36f6031464758309bb3f644d8d121
M_ELSaghier <thedreamereng@gmail.com>
2018-02-03 02:03:37 +0200

    Save data to real time data base

--- app/src/main/java/elsaghier/developer/com/capstoneproject/Activities/ToDoActivity.java
+++ app/src/main/java/elsaghier/developer/com/capstoneproject/Activities/ToDoActivity.java
@@ -6,10 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import elsaghier.developer.com.capstoneproject.Models.Film;
 import elsaghier.developer.com.capstoneproject.R;
 import elsaghier.developer.com.capstoneproject.ToDoDialog;
 
@@ -24,14 +20,6 @@ public class ToDoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_to_do);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
-        Film f = new Film();
-        f.setBackdropPath("fff00");
-        f.setId(3);
-        f.setGeneres("FDSF");
-        f.setOverview("GAGGGG");
-        myRef.push().setValue(f);
         doDialog = new ToDoDialog(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
--- app/src/main/java/elsaghier/developer/com/capstoneproject/ToDoDialog.java
+++ app/src/main/java/elsaghier/developer/com/capstoneproject/ToDoDialog.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.view.Window;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import elsaghier.developer.com.capstoneproject.Models.ProgressDialogClass;
 
 /**
  * Created by ELSaghier on 2/2/2018.
@@ -22,6 +25,8 @@ public class ToDoDialog extends Dialog {
     @BindView(R.id.message_TIL)
     TextInputLayout message;
 
+    Button save, cancel;
+
     public ToDoDialog(@NonNull Context context) {
         super(context);
     }
@@ -31,27 +36,33 @@ public class ToDoDialog extends Dialog {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.add_to_do_dialog);
-
+        ButterKnife.bind(this);
     }
 
     @OnClick(R.id.save_btn)
     void addTo_RT_DB() {
+        ProgressDialogClass.showProgressDialog(getContext(), "Save data", "Sending data to server");
         String m = message.getEditText().getText().toString();
         if (m.isEmpty())
             message.setError("Text can't be empty");
-        else
+        else {
             addToRealTimeDB(m);
-        dismiss();
-    }
-    @OnClick(R.id.cancel_btn)
-    void cancelDialog() {
+            message.getEditText().setText("");
+        }
+        ProgressDialogClass.hideProgressDialog();
         dismiss();
     }
 
-    void addToRealTimeDB(String message) {
+    @OnClick(R.id.cancel_btn)
+    void cancelDialog() {
+        dismiss();
+        message.getEditText().setText("");
+    }
+
+    private void addToRealTimeDB(String message) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference().child("messages");
-        myRef.setValue(message);
+        myRef.push().setValue(message);
     }
 
 }
